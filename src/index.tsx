d6b0b1a564b08ccf9326cea65f5b4dc93b98a255
hovhrachya93 <hovhrachya93@gmail.com>
2021-05-16 21:25:04 +0400

    types around events refs

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import ReactDOM from 'react-dom';
 import GuestList from './state/GuestList';
+// import UserSearch from './state/UserSearch';
+import UserSearch from './refs/UserSearch';
+import EventComponent from './events/EventComponent';
+
+
 
 const App = () => {
     return (
         <div>
             <GuestList />
+            <UserSearch />
+            <EventComponent />
         </div>
     )
 }
--- src/refs/UserSearch.tsx
+++ src/refs/UserSearch.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 const users = [
   { name: 'Sarah', age: 20 },
@@ -6,9 +6,17 @@ const users = [
   { name: 'Michael', age: 20 },
 ];
 
-const UserSearch = () => {
+const UserSearch: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null); 
   const [name, setName] = useState('');
   const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  useEffect(() =>{
+    if(!inputRef.current){
+      return;
+    }
+    inputRef.current.focus()
+  },[])
 
   const onClick = () => {
     const foundUser = users.find((user) => {
@@ -20,7 +28,7 @@ const UserSearch = () => {
   return (
     <div>
       User Search
-      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <input ref={inputRef} value={name} onChange={(e) => setName(e.target.value)} />
       <button onClick={onClick}>Find User</button>
       <div>{user && user.name}</div>
       <div>{user && user.age}</div>
